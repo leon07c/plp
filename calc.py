c64c0fbf0d87ec397d9af3c681cd8e8ec8c9d17e
leon07c <kabugileon1@gmail.com>
2025-08-03 21:03:03 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Invalid input. Please enter numeric values.")
+    exit()
+
+operation = input("Enter an operation (+, -, *, /): ")
+
+
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please enter one of +, -, *, /.")
